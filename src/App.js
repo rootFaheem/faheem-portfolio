847df99c0510a3af1c254a71f398219f12133ce8
Faheem <faheemtech1@gmail.com>
2019-02-21 21:12:59 +0530

    building from scratch! :)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 
-import Navbar from "./components/Layout/Navbar";
-import Footer from "./components/Layout/Footer";
-import Landing from "./components/Layout/Landing";
-
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
+        <h1>i am faheem profile</h1>
       </div>
     );
   }
