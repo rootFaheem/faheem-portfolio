38afb97a1c93df0965c308110ad174868a0e99df
Faheem <faheemtech1@gmail.com>
2019-02-21 23:56:49 +0530

    positioning fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Landing from "./components/Layout/Landing";
 
 import "./App.css";
 
@@ -6,8 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I am Faheem Ahmad</h1>
-        <h1>I am a Full Stack Developer</h1>
+        <Landing>
+          <div>
+            <h1>I am Faheem Ahmad</h1>
+            <h1>I am a Full Stack Developer</h1>
+          </div>
+        </Landing>
       </div>
     );
   }
---
+++ src/components/Layout/Landing.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return <div className="landing" />;
+  }
+}
+
+export default Landing;
