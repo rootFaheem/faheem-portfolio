752f75ed9eebffb37313c80c2dc4fca1d92ca706
Faheem <faheemtech1@gmail.com>
2019-02-21 23:30:57 +0530

    bg image is added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>i am faheem profile</h1>
+        <h1>I am Faheem Ahmad</h1>
+        <h1>I am a Full Stack Developer</h1>
       </div>
     );
   }
