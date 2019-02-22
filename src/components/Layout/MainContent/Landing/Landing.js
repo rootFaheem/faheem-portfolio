2213b98babb16824d14af3416270086cc6ee8283
Faheem <faheemtech1@gmail.com>
2019-02-22 23:07:38 +0530

    bg image added and fixed to perfect

--- src/components/Layout/MainContent/Landing/Landing.js
+++ src/components/Layout/MainContent/Landing/Landing.js
@@ -1,15 +1,20 @@
 import React from "react";
-
+import BackgroundImage from "../../../../assets/showcase.jpg";
 import "./Landing.css";
 
 const landing = () => {
   return (
-    <div className="Landing">
-      <h1>Landing Page! </h1>
-      <hr />
-      <p>
-        This is my landing page where i want to put some links the basic details{" "}
-      </p>
+    <div>
+      <img src={BackgroundImage} id="bg" alt="" />
+
+      <div className="Landing">
+        <h1>Landing Page! </h1>
+        <hr />
+        <p>
+          This is my landing page where i want to put some links the basic
+          details{" "}
+        </p>
+      </div>
     </div>
   );
 };
