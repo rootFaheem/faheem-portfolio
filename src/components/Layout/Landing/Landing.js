c1a80b52aeebec16f0612b001e6131b71693ee4b
Faheem <faheemtech1@gmail.com>
2019-02-22 11:38:23 +0530

    brand size fixed

--- src/components/Layout/Landing/Landing.js
+++ src/components/Layout/Landing/Landing.js
@@ -7,11 +7,14 @@ const landing = () => {
       <div className="dark-overlay landing-inner text-light">
         <div>
           <Navbar />
-          <a href="/">
-            <div className="brand">
-              <p>faheem</p>
-            </div>
-          </a>
+          <div className="brand">
+            <a href="/">
+              <div className="brand-name">
+                <p>faheem</p>
+              </div>
+            </a>
+          </div>
+
           <div className="Info">
             <h1>I am Faheem Ahmad</h1>
             <h1>I am a Full Stack Developer</h1>
