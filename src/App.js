40e4d0c2f1e9779fe98671c1e41613f90922f9cf
Faheem <faheemtech1@gmail.com>
2019-02-22 14:09:12 +0530

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import Landing from "./components/Layout/Landing/Landing";
-
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Landing />
+        <h1>i am faheem</h1>
       </div>
     );
   }
--- src/components/Layout/Landing/Navbar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import classes from "./Navbar.css";
-const navbar = () => {
-  return <div />;
-};
-
-export default navbar;
