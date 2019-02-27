ef60088cabb90d0478b29c0a1a94eadc7a9b6ce9
Faheem <faheemtech1@gmail.com>
2019-02-28 01:19:59 +0530

    fixed a bug

---
+++ src/components/Blog/Articles/Article/Article.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import react from "../../../../assets/Article/react.png";
+
+class Article extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-4">
+            <img src={react} alt="article showcase" className="imgShowcase" />
+          </div>
+          <div className="col-md-8">
+            <div className="row">
+              <div className="col-auto">
+                <Link to="/blog/react-intro">
+                  {" "}
+                  <p className="h3 heading">
+                    An introduction to React with Redux and axios i.e a http
+                    client
+                  </p>{" "}
+                </Link>
+              </div>
+            </div>
+            <div
+              className="row justify-content-between"
+              style={{ color: "#999999" }}
+            >
+              <div className="col-auto">
+                {" "}
+                <small>in </small>{" "}
+                <small style={{ color: "Green" }}>TOPIC</small>
+              </div>
+              <div className="col-auto">
+                <small>{this.updatedDate}</small>{" "}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-auto">
+                {" "}
+                <p style={{ color: "#293347" }}>
+                  In this article you will learn about basics of React, Redux.
+                  And will Learn how to build the reusable component. Before
+                  this you will learn how to setup a React+Redux Environmet.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+export default Article;
--- src/components/Blog/Articles/Articles.js
+++ src/components/Blog/Articles/Articles.js
@@ -1,87 +1,34 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
-
+import Article from "./Article/Article";
 import "./Articles.css";
-import Test from "../../../assets/showcase.jpg";
 
-class Article extends Component {
-  monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
-  d = new Date();
-
-  updatedDate =
-    this.monthNames[this.d.getMonth()] +
-    " " +
-    new Date().getDay() +
-    ", " +
-    new Date().getFullYear();
-
-  componentDidMount() {
-    let $ = window.$;
-    $(document).ready(function() {
-      $('[data-toggle="tooltip"]').tooltip();
-    });
-  }
-
+class Articles extends Component {
   render() {
     return (
       <div className="container my-container">
-        <div className="row my-row">
-          <div className="col-md-9 col-xs-auto my-col">
-            <div className="row">
-              <div className="col-md-4">
-                <img
-                  src={Test}
-                  alt="article showcase"
-                  className="imgShowcase"
-                />
-              </div>
-              <div className="col-md-8">
-                <div className="row">
-                  <Link to="/blog/react-intro">
-                    {" "}
-                    <p className="h3 heading">
-                      An introduction to React with Redux and axios i.e a http
-                      client
-                    </p>{" "}
-                  </Link>
-                </div>
-                <div className="row justify-content-between">
-                  <div className="col-auto">
-                    {" "}
-                    <small>in TOPIC</small>
-                  </div>
-                  <div className="col-auto">
-                    <small>{this.updatedDate}</small>{" "}
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div className="row ">
+          <div className="col-md-9 col-auto">
+            <Article />
+            <Article />
           </div>
 
-          <div className="col-md-3 col-xs-auto my-col">row 1 col 2</div>
-        </div>
-        <div className="row my-row">
-          <div className="col-sm-4 my-col">row 2 col 1</div>
-          <div className="col-sm-8 my-col">row 2 col 2</div>
+          <div
+            className="col-md-3 col-auto text-center"
+            style={{ color: "green" }}
+          >
+            <p>react (7)</p>
+            <p>redux (4)</p>
+            <p>node (5)</p>
+            <p>JavaScript (12)</p>
+            <p>Bootstrap (8)</p>
+            <p>Machine Learning (3)</p>
+            <p>Web Development (14)</p>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default Article;
+export default Articles;
