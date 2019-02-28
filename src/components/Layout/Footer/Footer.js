c1cb0c91896842f5a88d07544d3023abf5208c4f
Faheem <faheemtech1@gmail.com>
2019-03-01 03:44:56 +0530

    footer added with social links

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -6,68 +6,59 @@ const footer = () => {
     <div>
       <div className="stickyfooter">
         <footer className="page-footer font-small bg-dark pt-4 ">
-          <div className="container">
-            <ul className="list-unstyled list-inline text-center">
-              <li className="list-inline-item" href="www.twitter.com">
-                <a
-                  className="btn-floating btn-tw mx-1"
-                  id="footerlinks"
-                  href="https://twitter.com/rootfaheem"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-twitter"> </i>
-                </a>
-              </li>
-              <li className="list-inline-item" href="www.twitter.com">
-                <a
-                  className="btn-floating btn-github mx-1"
-                  id="footerlinks"
-                  href="https://github.com/FaheemTech"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-github"> </i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a
-                  className="btn-floating btn-google mx-1"
-                  id="footerlinks"
-                  href="mailto:faheemtech1@gmail.com?Subject=Hello%20faheem"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-google"> </i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a
-                  className="btn-floating btn-li mx-1"
-                  id="footerlinks"
-                  href="https://www.linkedin.com/in/faheem-ahmad-753705127/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-linkedin-in"> </i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a
-                  className="btn-floating btn-dribbble mx-1"
-                  id="footerlinks"
-                  href="https://dribbble.com/faheemtech"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-dribbble"> </i>
-                </a>
-              </li>
-            </ul>
+          <div className="container" />
+          <div className="row align-items-center">
+            <div className="col text-center ">
+              <a
+                href="https://twitter.com/rootfaheem"
+                id="textdecoration"
+                role="button"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-twitter" />
+              </a>
+              <a
+                href="mailto:faheemtech1@gmail.com?Subject=Hello%20faheem"
+                id="textdecoration"
+                role="button"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-google"> </i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/faheem-ahmad-753705127/"
+                id="textdecoration"
+                role="button"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-github" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/faheem-ahmad-753705127/"
+                id="textdecoration"
+                role="button"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-linkedin" />
+              </a>
+              <a
+                href="https://dribbble.com/faheemtech"
+                id="textdecoration"
+                role="button"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <i className="fa fa-dribbble" />
+              </a>
+            </div>
           </div>
-
-          <div className="footer-copyright text-center py-3" id="footerlinks">
-            <a className="footerlinks" id="footerlinks" href="/">
-              &copy; {new Date().getFullYear()} faheem
+          <div className="footer-copyright text-center" id="footerlinks">
+            <a className="footerlinks  text-center" id="footerlinks" href="/">
+              <small>&copy; {new Date().getFullYear()} faheem</small>
             </a>
           </div>
         </footer>
