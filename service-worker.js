c7ec554e6451c783675fc745b22af26cb2b55335
Faheem <faheemtech1@gmail.com>
2019-03-27 17:48:08 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/faheem-profile/precache-manifest.ceb336cdd0739470a7bea7ca87ead19f.js"
+  "/faheem-ahmad/precache-manifest.072e29a812a8dd86c4c5b43471307361.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/faheem-profile/index.html", {
+workbox.routing.registerNavigationRoute("/faheem-ahmad/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
