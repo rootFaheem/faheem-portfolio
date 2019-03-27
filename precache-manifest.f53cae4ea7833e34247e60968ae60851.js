947a437ff09b6eeba633eb0bb9e6b42d1ab60a21
Faheem <faheemtech1@gmail.com>
2019-03-27 17:33:34 +0530

    Updates

---
+++ precache-manifest.f53cae4ea7833e34247e60968ae60851.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc511014f6f44a6e63b7f1bf034caf12",
+    "url": "/FaheemTech/faheem-portfolio/static/media/backmyitem.bc511014.png"
+  },
+  {
+    "revision": "3e03d297aaeacb1cfe6d",
+    "url": "/FaheemTech/faheem-portfolio/static/css/main.27f0254c.chunk.css"
+  },
+  {
+    "revision": "9cccba716d936b02511f",
+    "url": "/FaheemTech/faheem-portfolio/static/js/runtime~main.9cccba71.js"
+  },
+  {
+    "revision": "e9df0827cbdb1ad093b5d4ada419a02b",
+    "url": "/FaheemTech/faheem-portfolio/static/media/faheem_logo.e9df0827.png"
+  },
+  {
+    "revision": "33bd5569c66b56957133",
+    "url": "/FaheemTech/faheem-portfolio/static/js/2.33bd5569.chunk.js"
+  },
+  {
+    "revision": "1cfa26fa22930726bb3d332591d707bf",
+    "url": "/FaheemTech/faheem-portfolio/static/media/nextgenjs.1cfa26fa.jpeg"
+  },
+  {
+    "revision": "79279ed4c84489e3a19910c16d2bba04",
+    "url": "/FaheemTech/faheem-portfolio/static/media/faheem.79279ed4.png"
+  },
+  {
+    "revision": "adae8abfa8a4cc32aa0a2373abab18cd",
+    "url": "/FaheemTech/faheem-portfolio/static/media/Faheem_Resume.adae8abf.pdf"
+  },
+  {
+    "revision": "3fdcfc6885bc3f8f095a5a7802224908",
+    "url": "/FaheemTech/faheem-portfolio/static/media/BurgerBuilder.3fdcfc68.png"
+  },
+  {
+    "revision": "3e03d297aaeacb1cfe6d",
+    "url": "/FaheemTech/faheem-portfolio/static/js/main.3e03d297.chunk.js"
+  },
+  {
+    "revision": "6977e454ec80692936f479b948cef9eb",
+    "url": "/FaheemTech/faheem-portfolio/static/media/nodeshop.6977e454.png"
+  },
+  {
+    "revision": "68a83cd59ab67c66bb4d8bad2d1382f3",
+    "url": "/FaheemTech/faheem-portfolio/static/media/save4thpillar.68a83cd5.png"
+  },
+  {
+    "revision": "c41405a087b3ff8265a392595c93c934",
+    "url": "/FaheemTech/faheem-portfolio/static/media/faheemprofile.c41405a0.png"
+  },
+  {
+    "revision": "44c81ac0858dc68d9e190929dccd752c",
+    "url": "/FaheemTech/faheem-portfolio/static/media/wsn.44c81ac0.png"
+  },
+  {
+    "revision": "8c9bde5fc926423f1c661bd1dfaef0a7",
+    "url": "/FaheemTech/faheem-portfolio/static/media/react.8c9bde5f.png"
+  },
+  {
+    "revision": "ed1889be2c05028ff74d6608f80ca2a3",
+    "url": "/FaheemTech/faheem-portfolio/static/media/nodeexpress.ed1889be.png"
+  },
+  {
+    "revision": "33bd5569c66b56957133",
+    "url": "/FaheemTech/faheem-portfolio/static/css/2.3909642b.chunk.css"
+  },
+  {
+    "revision": "fb9cd6d4a77772543547de84222fd27f",
+    "url": "/FaheemTech/faheem-portfolio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/FaheemTech/faheem-portfolio/precache-manifest.f53cae4ea7833e34247e60968ae60851.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/FaheemTech/faheem-portfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
